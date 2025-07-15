ea254937fadcfdd61cec85d177edfcb2dd30e013
Yashwant sahu <yashsahu3333@gmail.com>
2025-07-15 05:32:58 +0000

    Project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import netflix from "../../Assets/Projects/image copy 2.png";
+import blood from "../../Assets/Projects/image.png";
+import ecom from "../../Assets/Projects/image copy.png";
+import music from "../../Assets/Projects/image copy 3.png";
+import javascript from "../../Assets/Projects/javaScript.webp";
+import react from "../../Assets/Projects/react-logo@3x.svg";
 
 function Projects() {
   return (
@@ -23,67 +23,66 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={music}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Music Streaming Website"
+              description="I developed a music streaming application using React.js, Node.js, Express.js, MongoDB, and Material UI. The platform includes playlist creation, secure user authentication, and an interactive UI, designed with Material UI for a seamless and engaging user experience."
+              ghLink="https://github.com/yashwant2002/MERN-MusicApp.git"
+              demoLink="https://mern-musicapp-frontend.onrender.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={netflix}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Netflix Clone"
+              description="I built a Netflix-inspired streaming platform using React.js, Node.js, Express.js, MongoDB, Material UI, and Tailwind CSS. It features a dynamic UI, movie recommendations, user authentication, and TMDB API integration for trending content, ensuring a seamless and responsive user experience."
+              ghLink="https://github.com/yashwant2002/MERN-Neflix.git"
+              // demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={ecom}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="E-commerce website"
+              description="I built an e-commerce platform using React.js, Node.js, Express.js, MongoDB, Tailwind CSS, and Razorpay API, featuring user authentication, product management, payment integration, a real-time cart system, order tracking, and a responsive UI/UX designed with Tailwind CSS and Material UI."
+              ghLink="https://github.com/yashwant2002/Ecom-MERN.git"
+              // demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={react}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="React Project"
+              description="I have built several projects using React, including a weather application and a counter app. These projects helped me strengthen my understanding of React concepts such as components, state management, props, and API integration. Through hands-on experience, I gained confidence in building responsive and interactive user interfaces"
+              ghLink="https://github.com/yashwant2002/React.js.git"
+              // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={javascript}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Javascript Project"
+              description="I have built several projects using JavaScript and React, such as a weather application and a counter app. These projects helped me gain hands-on experience with JavaScript fundamentals like DOM manipulation, event handling, and asynchronous programming, as well as React-specific concepts such as components, state management, props, and API integration. Working on these projects has improved my ability to create interactive and user-friendly web applications"
+              ghLink="https://github.com/yashwant2002/Javascirpt.git"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={blood}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Blood Link"
+              description="LifeFlow is a blood donation platform built with the MERN stack (MongoDB, Express, React, Node.js) that connects donors, receivers, and hospitals. Users can register as donors or receivers with their profile details. The app supports real-time blood requests filtered by blood group and location using the Geolocation API. It tracks donation history, includes an admin panel for hospital staff, and sends email notifications to nearby donors for urgent needs"
+              ghLink="https://github.com/yashwant2002/Bloodlink.git"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
